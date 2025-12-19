95b5ac63c967914eea3ebf44d3653099fcd97386
Luiz Sabino <sabino.luiz@gmail.com>
2025-12-19 15:27:00 -0300

    19-12-2025

---
+++ index.js
@@ -0,0 +1,10 @@
+const hoje = new Date();
+  const opcoes = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  const dataExtenso = hoje.toLocaleDateString('pt-BR', opcoes);
+  document.getElementById('data-hoje').textContent = "Hoje é "+dataExtenso;
